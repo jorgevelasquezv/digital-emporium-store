09ca3931843c9df4df1c86e735389279b9c7fd8a
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2023-11-21 21:07:33 -0500

    Se crea pagina privacy policy

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,120 @@
+import Link from 'next/link';
+
+const page = () => {
+    return (
+        <section className="container mx-auto mt-8 p-4 bg-white rounded-lg shadow-lg">
+            <p className="text-3xl font-bold mb-4">
+                <strong>PRIVACY POLICY</strong>
+            </p>
+            <p className="mb-4">
+                This Privacy Policy sets forth the terms under which Digital
+                Emporium Store uses and protects the information that is
+                provided by its users when using its website. This company is
+                committed to the security of its users' data. When we ask you to
+                provide personal information by which you can be identified, we
+                do so with the assurance that it will only be used in accordance
+                with the terms of this document. However, this Privacy Policy
+                may change over time or be updated, so we recommend and
+                emphasize that you continually review this page to ensure that
+                you agree with the terms of this Privacy Policy. page to ensure
+                that you agree to any such changes.
+            </p>
+            <p className="text-xl font-bold mb-2">
+                <strong>Information that is collected</strong>
+            </p>
+            <p className="mb-4">
+                Our website may collect personal information such as for
+                example: Name, contact information such as your email address
+                and demographic information. Also also, when necessary, specific
+                information may be required to process an order information may
+                be required to process an order or make a delivery or billing.
+                billing.
+            </p>
+            <p className="text-xl font-bold mb-2">
+                <strong>Use of information gathered</strong>
+            </p>
+            <p className="mb-4">
+                Our website uses the information in order to provide the best
+                possible service, particularly to keep track of users, to keep
+                track of orders if applicable, and to improve our products and
+                services. We may periodically send e-mails through our site with
+                special offers, new products and other marketing information
+                that we deem relevant. and other advertising information that we
+                feel is relevant to you or may be of benefit to you. you or that
+                may be of benefit to you, these emails will be sent to the
+                address you these e-mails will be sent to the address you
+                provide and may be and may be unsubscribed at any time.
+            </p>
+            <p className="mb-4">
+                Digital Emporium Store is highly committed to fulfilling our
+                commitment to keeping your information secure. We use the most
+                advanced systems and constantly update them to ensure that there
+                is no unauthorized access.
+            </p>
+            <p className="text-xl font-bold mb-2">
+                <strong>Cookies</strong>
+            </p>
+            <p className="mb-4">
+                A cookie refers to a file that is sent with the purpose of
+                requesting permission to be stored on your computer, by
+                accepting this file is created and the cookie is then used to
+                have information regarding web traffic, and also facilitates
+                future visits to a recurring website. Another function that
+                cookies have is that with them the web can recognize you
+                individually and therefore provide you with the best
+                personalized service on its website.
+            </p>
+            <p className="mb-4">
+                Our website uses cookies to identify which pages are visited and
+                their frequency. This information is used only for statistical
+                analysis and then the information is permanently deleted. You
+                can delete cookies at any time from your computer. However,
+                cookies help to provide a better service from websites, they do
+                not give access to information about your computer or you,
+                unless you choose to provide it directly. You can accept or deny
+                the use of cookies, however most browsers automatically accept
+                cookies as it serves to have a better web service. You can also
+                change the settings on your computer to decline cookies. If you
+                decline cookies you may not be able to use some of our services.
+            </p>
+            <p className="text-xl font-bold mb-2">
+                <strong>Third Party Links</strong>
+            </p>
+            <p className="mb-4">
+                This website may contain links to other sites that may be of
+                interest to you. Once you click on these links and leave our
+                site, we no longer have control over the site to which you are
+                redirected and therefore we are not responsible for the terms or
+                privacy or data protection practices of such other sites. Those
+                sites are subject to their own privacy policies and we recommend
+                that you check with them to confirm that you agree with them.
+            </p>
+            <p className="text-xl font-bold mb-2">
+                <strong>Control de su información personal</strong>
+            </p>
+            <p className="mb-4">
+                At any time you may restrict the collection or use of personal
+                information that is provided to our{' '}
+                <Link href="" target="_blank" className="text-blue-700">
+                    website
+                </Link>
+                . Whenever you are asked to fill out a form, such as a user
+                registration form, you can check or uncheck the option to
+                receive information by e-mail. In case you have checked the
+                option to receive our newsletter or advertising you can cancel
+                it at any time.
+            </p>
+            <p className="mb-4">
+                This company will not sell, give away or distribute the personal
+                information that is collected without your consent, unless
+                required by a judge with a court order.
+            </p>
+            <p className="mb-4">
+                Digital Emporium Store reserves the right to change the terms of
+                this Privacy Policy at any time.
+            </p>
+        </section>
+    );
+};
+
+export default page;
